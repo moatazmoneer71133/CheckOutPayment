771ea9e087b0ec3d73f8b7f706ff0814082fbc5d
moatazmoneer71133 <moatazalf711@gmail.com>
2025-10-13 00:48:41 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/chack_out_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chack_out_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:chack_out_app/features/CheckOut/Presentation/view/My_Cart_View.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const CheckOutApp());
+}
+
+class CheckOutApp extends StatelessWidget {
+  const CheckOutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyCartView(),
+    );
+  }
+}
